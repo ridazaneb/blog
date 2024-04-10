28382be26a2344b373a70344612df2c1e0c99e47
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-04-09 20:59:38 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Define root route to articles#index
   root "articles#index"
 
-  # Define routes for articles resource
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-
-  # Define health check route
-  get "/up", to: "rails/health#show", as: :rails_health_check
+  resources :articles
 end
