4e9a112090d1235e3b4399237fbc0d0da9339c81
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-04-09 20:42:41 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-  root "articles#index"
-
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   Rails.application.routes.draw do
@@ -22,4 +17,10 @@ end
 
   # Defines the root path route ("/")
   # root "posts#index"
+end
+Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
