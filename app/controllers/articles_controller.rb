562d8cf7097073d3849fcee6954c0034b3b3aa19
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-04-10 00:16:11 -0400

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  # snippet for brevity
+end
   def index
     @articles = Article.all
   end
